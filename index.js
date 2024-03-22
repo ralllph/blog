7fb803e6f70f478e7212740d8abac2bcf74be149
mac <mac@macs-MacBook-Air.local>
2024-03-22 12:07:13 -0400

    sett up routes and mongo db

--- index.js
+++ index.js
@@ -11,5 +11,57 @@ getApp.set('view engine', 'ejs');
 //use body parser
 getApp.use(express.urlencoded({extended:true}));
 
+// Setup Database Connection
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/blog', {
+    UseNewUrlParser : true,
+    UseUnifiedTopology : true
+});
+
+// Setup Database Model
+const Post = mongoose.model('post', {
+
+});
+
+
+//routes 
+myApp.post('/', [
+    check('name', 'Name is required!').notEmpty(),
+    check('email', 'Please enter valid email address!').isEmail(),
+    check('phone','').custom(customPhoneValidation),
+    check('lunch').custom(customLunchAndTicketValidation)
+], function(req,res){
+    const errors = validationResult(req);
+    console.log(errors);
+
+    if(!errors.isEmpty()) {
+       
+    }
+    else 
+    {
+       
+        // Display Output
+        var pageData = {
+  
+        };
+
+        // Save Form Data into Database
+        let myPosts = new Post(pageData);
+        myOrder.save().then(function(){
+            console.log("New Post Created Successfully!");
+        }).catch(function(ex){
+            console.log(`Database Error: ${ex.toString()}`);
+        });
+
+        // Display Form Output
+        res.render('', pageData); 
+    }   
+});
+
+// Set Different Routes (Pages)
+getApp.get('/',function(req,res) {
+    res.render('');     // No need to add .ejs extension
+});
+
 //validation checker
 const {check, validationResult} = require('express-validator');
